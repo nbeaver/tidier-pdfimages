b992915e6d56df462ec7560b21818f92f685acb6
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-03-02 18:41:33 -0600

    Substantial improvements in handling file paths.

--- pdfimages-tidy.sh
+++ pdfimages-tidy.sh
@@ -15,7 +15,7 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ $1 == '-h' -o $1 == '-help' -o $1 == '--help' -o $1 == '-?' ]; then
-    echo "Usage: pdfimages-tidy [options] file.pdf"
+    echo "Usage: pdfimages-tidy [options] /optional/path/to/file.pdf"
     echo "This is a wrapper script for pdfimages."
     echo "pdfimages-tidy can use any options that pdfimages does,"
     echo "but it automatically sets the image-root name to be the"
@@ -32,20 +32,32 @@ if [ $1 == '-v' -o $1 == '-version' -o $1 == '--version' ]; then
 fi
 
 # TODO: Fix this so that filenames can have spaces.
-FILE_NAME="${@: -1}" # last argument (split by spaces) is the name of the pdf file.
+# DONE: Fix this so the script can run on pdfs outside of the working directory.
+FILEPATH="${@: -1}" # last argument (split by spaces) is the name of the pdf file.
 # http://stackoverflow.com/questions/1853946/getting-the-last-argument-passed-to-a-shell-script
 
-# FILE_NAME="${!#}"   # this should work, too.
-# http://stackoverflow.com/a/1216239/1608986
-
-ALL_BUT_FILE_NAME="${@:1:$(($#-1))}"
+#DONE: pass these to the script later on
+OTHER_ARGUMENTS="${@:1:$(($#-1))}"
 # http://stackoverflow.com/a/1215592/1608986
 
-IMAGE_FOLDER=$FILE_NAME"_images"
+# FILEPATH="${!#}"   # this should work, too.
+# http://stackoverflow.com/a/1216239/1608986
+
+FILE_DIR=$(dirname $FILEPATH)
+FILENAME=$(basename $FILEPATH)
+
+IMAGE_FOLDER=$FILEPATH"_images"
+
+# If the directory doesn't exist, then create it.
+if [ ! -d "$IMAGE_FOLDER" ]; then
+    mkdir "$IMAGE_FOLDER"
+fi
+# http://stackoverflow.com/questions/59838/how-to-check-if-a-directory-exists-in-a-shell-script
 
 DEBUG=0
 if [ $DEBUG -eq 1 ]; then
-    echo -e "Extracting images from file $FILENAME to folder $IMAGE_FOLDER by running this command:\n$ pdfimages \"../$FILE_NAME\" \"$FILE_NAME\""
+    echo -e "Extracting images from file $FILENAME to folder $IMAGE_FOLDER by running this command:"
+    echo -e  pdfimages $OTHER_ARGUMENTS "$FILEPATH" "$IMAGE_FOLDER/$FILENAME"
     # http://stackoverflow.com/questions/226703/how-do-i-prompt-for-input-in-a-linux-shell-script
     select CHOICE in "Yes" "No"; do
         case $CHOICE in
@@ -55,22 +67,14 @@ if [ $DEBUG -eq 1 ]; then
     done
 fi
 
-# If the directory doesn't exist, then create it.
-if [ ! -d "$IMAGE_FOLDER" ]; then
-    mkdir "$IMAGE_FOLDER"
-fi
-# http://stackoverflow.com/questions/59838/how-to-check-if-a-directory-exists-in-a-shell-script
-
-# Because pdfimages dumps into the current directory,
-# we have to go into the image folder and then reference the parent directory.
-cd "$IMAGE_FOLDER"
 # Run pdfimages with the necessary flags, and use the filename as the starting point for the images names.
 # e.g. file.pdf-000.ppm, file.pdf-001.ppm, ...
 echo -n "Extracting images..."
-pdfimages "../$FILE_NAME" "$FILE_NAME"
+pdfimages $OTHER_ARGUMENTS "$FILEPATH" "$IMAGE_FOLDER/$FILENAME"
 echo "done."
-number_of_images_extracted=$(find ./ -maxdepth 1 -type f -print| wc -l)
+number_of_images_extracted=$(find $IMAGE_FOLDER -maxdepth 1 -type f -print| wc -l)
 echo "$number_of_images_extracted images extracted."
+cd "$IMAGE_FOLDER"
 # automatically run pnmtopng on the files that end with ppm.
 # http://www.cyberciti.biz/faq/bash-loop-over-file/
 shopt -s nullglob
@@ -93,4 +97,3 @@ do
     filename_no_extension="${pbm_file%.*}"
     pnmtopng "$pbm_file" > "$filename_no_extension.png"
 done
-cd - &> /dev/null # go back up to the starting folder, throwing away the output, which is just the name of the directory we started in.
